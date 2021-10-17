c219790aa14b01392baa98559826c2955d930ca3
leonardogavaudan <leonardogavaudan@gmail.com>
2021-10-17 17:02:03 +0200

    updated gitignore to include package.json file, and added database connection feature

--- express.js
+++ express.js
@@ -1,14 +1,28 @@
 const express = require('express');
 const path = require('path');
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3000;
 
-app.use('/newProfile.html', require('./routes/newProfile'))
-
-// Serve content
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
-    console.log(`listening at http://127.0.0.1:${port}`)
-})
+    console.log(`listening at http://127.0.0.1:${port}`);
+});
+
+// Database
+const db = mysql.createConnection({
+    host:'localhost',
+    user: 'testuser',
+    password: 'testpassword',
+    database: 'dbfriends'
+});
+
+db.connect();
+module.exports = {db};
+
+// Routes
+app.use('/newProfile/', require('./routes/newProfile'));
+app.use(express.static(path.join(__dirname, 'public')));
--- routes/newProfile.js
+++ routes/newProfile.js
@@ -2,8 +2,17 @@ const express = require('express');
 const uuid = require('uuid');
 const router = express.Router();
 
+const {db} = require('../express.js');
+
 router.post('/', (req, res) => {
-    console.log("got hereeee")
+    console.log("got hereeee");
+    // res.send(req.body)
+
+    const sql = `SELECT * FROM users;`;
+
+    db.query(sql, (error, results) => {
+        res.send(results);
+    })
 })
 
 module.exports = router;
