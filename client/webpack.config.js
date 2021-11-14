c357ba0216e5629f17be5062973614ce7700da78
leonardogavaudan <leonardogavaudan@gmail.com>
2021-11-14 23:59:54 +0100

    added react preset and some other studdf

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,22 +1,24 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/main.js',
+    entry: './src/main.jsx',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
     mode: 'production',
-    rules: [
-        {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
                 }
             }
-        }
-    ]
+        ]
+    }
 };
