bd6bbe8b1ab0e48ca5779106885876c59b8ec453
leonardogavaudan <leonardogavaudan@gmail.com>
2022-02-08 20:34:31 +0100

    added dark mode

--- client/styles/theme.ts
+++ client/styles/theme.ts
@@ -1,23 +1,11 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
-    text: {
-      primary: '#ffffff',
-    },
-    background: {
-      default: '#121212',
-    },
+    mode: 'dark',
     primary: {
-      main: '#282a36',
-    },
-    secondary: {
-      main: '#282a36',
-    },
-    error: {
-      main: red.A400,
+      main: '#fff',
     },
   },
 });
