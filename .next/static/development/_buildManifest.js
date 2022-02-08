e291fe300c918e3537de95be3425d10401d90c65
leonardogavaudan <leonardogavaudan@gmail.com>
2022-02-08 22:28:19 +0100

    removed .next folder and updated gitignore

--- .next/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/new_contact":["static\u002Fchunks\u002Fpages\u002Fnew_contact.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fnew_contact"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
